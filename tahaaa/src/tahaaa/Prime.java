4cf8eb99b483bb4469f0be9860e6c26a033b65c0
BlackStone0 <97619827+BlackStone0@users.noreply.github.com>
2023-01-20 11:40:05 +0500

    Add files via upload

---
+++ tahaaa/src/tahaaa/Prime.java
@@ -0,0 +1,16 @@
+package tahaaa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Prime {
+
+	@Test
+	void test() {
+		main prime = new main();
+		boolean check = prime.CheckingPrime(2);
+		assertEquals(true,check);
+	}
+
+}
---
+++ tahaaa/src/tahaaa/main.java
@@ -0,0 +1,19 @@
+package tahaaa;
+
+public class main {
+
+	public boolean CheckingEven(int num) {
+		if(num % 2 == 0)
+			return true;
+			return false;
+	}
+	
+	public boolean CheckingPrime(int num) {
+		boolean isPrime = true;
+		
+		if(num==1) {
+			isPrime = false;			
+		}
+		return false;
+	}
+}
